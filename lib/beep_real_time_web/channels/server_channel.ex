bcaa42e0912fbfce55d8cbba20239b467af403a7
duratm <duratmathias@gmail.com>
2026-01-22 19:05:54 +0100

    feat(servers-presence): add server presence

--- lib/beep_real_time_web/channels/server_channel.ex
+++ lib/beep_real_time_web/channels/server_channel.ex
@@ -1,6 +1,7 @@
 defmodule BeepRealTimeWeb.ServerChannel do
   use Phoenix.Channel
   alias BeepRealTimeWeb.ChannelAuth
+  alias BeepRealTimeWeb.Presence
 
   # A user connects to this channel to receive notifications aimed at this server
   @impl true
@@ -9,11 +10,26 @@ defmodule BeepRealTimeWeb.ServerChannel do
     case ChannelAuth.require_user_channel(socket) do
       :ok ->
         # TODO: Authorize that the user can access this server
+        send(self(), :after_join)
         {:ok, socket}
 
       {:error, reason} ->
         {:error, reason}
     end
+  end
+
+  @impl true
+  def handle_info(:after_join, socket) do
+    user_id = socket.assigns[:user_id]
+    {:ok, _} = Presence.track(socket, user_id, %{online_at: inspect(System.system_time(:second)), user_id: user_id})
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "presence_diff", payload: diff}, socket) do
+    broadcast(socket, "presence_diff", diff)
+    {:noreply, socket}
   end
 
   @impl true
